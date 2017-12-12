2495c137bb40d756818e626f20819f694300fe6c
Dave <dave@dave.dave>
2017-12-12 19:24:48 +0000

    Completed day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,19 +1,21 @@
 extern crate petgraph;
 extern crate regex;
 
-use petgraph::{Graph};
+use std::fs::File;
+use std::io::{BufRead,BufReader};
+use std::collections::HashMap;
+
 use petgraph::graph::node_index;
 use petgraph::prelude::*;
 use petgraph::visit::{Walker,Dfs};
-use petgraph::EdgeDirection::{Outgoing, Incoming};
-use std::fs::File;
-use std::io::{BufRead,BufReader};
+use petgraph::EdgeDirection::Outgoing;
+
 use regex::Regex;
-use std::collections::HashMap;
+
 
 fn main() {
 
-    let f = BufReader::new(File::open("test.txt").unwrap());
+    let f = BufReader::new(File::open("input.txt").unwrap());
     let data: Vec<String> = f.lines()
                             .map(|l| l.unwrap())
                             .collect();
@@ -24,10 +26,9 @@ fn main() {
 
     let mut map = HashMap::new();
 
-
-
-    // Iterate over list, add required no. of nodes to graph,
-    // collect connections to be made
+    // Iterate over input data, add required no. of nodes to graph,
+    // collect connections to be made in tuples and store in
+    // hashmap for later retrieval
     for line in &data {
         let after = re_pipe.replace_all(line, ", ");
 
@@ -45,10 +46,9 @@ fn main() {
         map.insert(conn[0], conn_node_vec);
     }
 
-    let mut remaining_nodes: Vec<usize> = (0..network.node_count()).collect();
-    //println!("rn {:?}", remaining_nodes);
-    // Create connections in graph by reading data stored in
-    // hashmap
+    // Create connections in graph by iterating over all data
+    // stored in hashmap and applying the connection tuple for
+    // each node (if it is not a singleton)
     for (_, conn_vec) in map.iter() {
         for conn in conn_vec {
             if conn.0 != conn.1 {
@@ -57,41 +57,48 @@ fn main() {
         }
     }
     
-    // Step through all nodes connected to node 0 until return none
+    // Step through all nodes connected to node 0 until none left
     let stepper = Dfs::new(&network, node_index(0));
     let nodes_connected_to_0: Vec<NodeIndex> = stepper.iter(&network).collect();
 
     println!("nodes connected to node 0 [part1]: {:?}", nodes_connected_to_0.len());
 
-    let mut num_groups = 0;
+    /* Part 2 */
+    let mut num_groups = 0; // counter for number of separate sub-networks
 
-    let singletons: Vec<NodeIndex> = network.externals(Outgoing).collect();
+    // Build a vector containing all the nodes in the network
+    let mut remaining_nodes: Vec<NodeIndex> = (0..network.node_count())
+                                                .map(|x| node_index(x))
+                                                .collect();
 
-    num_groups += singletons.len();
+    // Remove the nodes we have already accounted for, leaving in the
+    // network those nodes that we haven't stepped into yet
+    get_nodes_remaining(&mut remaining_nodes, &nodes_connected_to_0);
 
-    println!("No. of singletons: {:?}", singletons.len());
-    
-    for single_node in singletons {
-        network.remove_node(single_node);    
-    }
-
-
-    for node in nodes_connected_to_0 {
-        network.remove_node(node);
-    }
-
+    // Increment num_groups counter as we have removed the network from part 1
     num_groups += 1;
 
-    while network.node_count() > 0 {
+    // Find and remove nodes which have no connections to any other nodes.
+    // Each of these nodes counts as a group
+    let singletons: Vec<NodeIndex> = network.externals(Outgoing).collect();
+    get_nodes_remaining(&mut remaining_nodes, &singletons);
+    num_groups += singletons.len();
 
+    // Loop over list of remaining nodes, finding full sub-networks for
+    // each and removing these sub-networks from the remaining node list.
+    while !remaining_nodes.is_empty() {
+        let stepper = Dfs::new(&network, *remaining_nodes.first().unwrap());
+        let sub_network: Vec<NodeIndex> = stepper.iter(&network).collect();
+        get_nodes_remaining(&mut remaining_nodes, &sub_network);
+        num_groups += 1;
     }
-    
 
-    //let nodes_in_g0 = 
+    println!("Total number of groups [part2]: {:?}", num_groups);
+}
 
+fn get_nodes_remaining (
+    remaining_nodes: & mut Vec<NodeIndex>,
+    nodes_to_remove: &Vec<NodeIndex>) {
 
-    //
-    //let r = network.without_edges(Outgoing).count();
-    //println!("r{:?}", r);
-
+    remaining_nodes.retain(|node| !nodes_to_remove.contains(node));
 }
