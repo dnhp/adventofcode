efacaa60349bfc3aade95dfe4ac8d1e97f7460e0
Dave <dave@dave.dave>
2017-12-20 22:35:43 +0000

    Completed day 20, kicked my arse. Shouldn't have tried to get smart with the quadratic formula

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,29 +1,24 @@
 extern crate regex;
 extern crate nalgebra;
+extern crate integer_sqrt;
 
 use std::fs::File;
 use std::io::{BufRead,BufReader};
 use regex::Regex;
-use nalgebra::{DMatrix3,Determinant,Matrix3};
+use nalgebra::{Determinant,Matrix3};
 
 fn main() {
 
-    let input_file = File::open("test.txt").unwrap();
+    let input_file = File::open("input.txt").unwrap();
     let buf = BufReader::new(input_file);
-
-    // let re_pos = Regex::new(r"p=<(-?\d+),(-?\d+),(-?\d+)>").unwrap();
-    // let re_vel = Regex::new(r"v=<(-?\d+),(-?\d+),(-?\d+)>").unwrap();
-    //let re_acc = Regex::new(r"a=<(-?\d+),(-?\d+),(-?\d+)>").unwrap();
+;
     let re = Regex::new(r"p=<(-?\d+),(-?\d+),(-?\d+)>, v=<(-?\d+),(-?\d+),(-?\d+)>, a=<(-?\d+),(-?\d+),(-?\d+)>").unwrap();
-
-    let mut max_manhat_dist: (usize, isize) = (0, 0);
-    let mut min_manhat_dist: (usize,isize) = (0,999999);
 
     let mut pos_vec: Vec<(isize,isize,isize)> = Vec::new();
     let mut vel_vec: Vec<(isize,isize,isize)> = Vec::new();
     let mut acc_vec: Vec<(isize,isize,isize)> = Vec::new();
 
-    for (p_id,line) in buf.lines().map(|l| l.unwrap()).enumerate() {
+    for line in buf.lines().map(|l| l.unwrap()) {
 
         let caps = re.captures(&line).unwrap();
 
@@ -40,78 +35,89 @@ fn main() {
         let acc: (isize,isize,isize) = (caps.get(7).unwrap().as_str().parse().unwrap(), 
                                         caps.get(8).unwrap().as_str().parse().unwrap(),
                                         caps.get(9).unwrap().as_str().parse().unwrap());
-
         acc_vec.push(acc);
-        //println!("caps: {:?}", acc);
-
-/*        let mat: DMatrix3<isize> = DMatrix3::new_zeros(3,3);
-        println!("mat: {:?}",mat );
-
-        let dist = acc.0.abs() + acc.1.abs() + acc.2.abs();
-        if dist > max_manhat_dist.1 {
-            max_manhat_dist.0 = p_id;
-            max_manhat_dist.1 = dist;
-        }
-        else if dist < min_manhat_dist.1 {
-            min_manhat_dist.0 = p_id;
-            min_manhat_dist.1 = dist;
-        }*/
-
     }
-    println!("pos vec: {:?}", pos_vec);
 
-    let mut remaining_pids: Vec<usize> = (0..1000).collect();
 
-    let mut mat: DMatrix3<isize> = DMatrix3::new_zeros(3,3);
+    let mut collision_t_matrix: Vec<Vec<isize>> = vec![vec![isize::max_value(); pos_vec.len()]; pos_vec.len()];
 
-    println!("Biggest accel: {:?}", min_manhat_dist);
+    for p1 in 0..pos_vec.len()-1 {
 
-    for p1 in 0..1 {
-        for p2 in p1+1..2 {
-            /*let delta_r = vec![pos_vec[p1].0-pos_vec[p2].0, 
-                               pos_vec[p1].1-pos_vec[p2].1,
-                               pos_vec[p1].2-pos_vec[p2].2];
+        for p2 in p1+1..pos_vec.len() {
 
-            let delta_v = vec![vel_vec[p1].0-vel_vec[p2].0, 
-                               vel_vec[p1].1-vel_vec[p2].1,
-                               vel_vec[p1].2-vel_vec[p2].2];
+            let m2 = Matrix3::new(
+                              2*(pos_vec[p1].0-pos_vec[p2].0),
+                              2*(vel_vec[p1].0-vel_vec[p2].0) + acc_vec[p1].0-acc_vec[p2].0,
+                              acc_vec[p1].0-acc_vec[p2].0,
 
-            let delta_a = vec![acc_vec[p1].0-acc_vec[p2].0, 
-                               acc_vec[p1].1-acc_vec[p2].1,
-                               acc_vec[p1].2-acc_vec[p2].2]; */
-
-            let delta_vec = vec![pos_vec[p1].0-pos_vec[p2].0, 
-                               pos_vec[p1].1-pos_vec[p2].1,
-                               pos_vec[p1].2-pos_vec[p2].2,
-
-                              vel_vec[p1].0-vel_vec[p2].0, 
-                              vel_vec[p1].1-vel_vec[p2].1,
-                              vel_vec[p1].2-vel_vec[p2].2,
-
-                              acc_vec[p1].0-acc_vec[p2].0, 
+                              2*(pos_vec[p1].1-pos_vec[p2].1),
+                              2*(vel_vec[p1].1-vel_vec[p2].1) + acc_vec[p1].1-acc_vec[p2].1,
                               acc_vec[p1].1-acc_vec[p2].1,
-                              acc_vec[p1].2-acc_vec[p2].2];    
-                              println!("vec: {:?}",delta_vec ); 
 
-            let m2 = Matrix3::new(pos_vec[p1].0-pos_vec[p2].0, 
-                               pos_vec[p1].1-pos_vec[p2].1,
-                               pos_vec[p1].2-pos_vec[p2].2,
-
-                              vel_vec[p1].0-vel_vec[p2].0, 
-                              vel_vec[p1].1-vel_vec[p2].1,
-                              vel_vec[p1].2-vel_vec[p2].2,
-
-                              acc_vec[p1].0-acc_vec[p2].0, 
-                              acc_vec[p1].1-acc_vec[p2].1,
+                              2*(pos_vec[p1].2-pos_vec[p2].2),
+                              2*(vel_vec[p1].2-vel_vec[p2].2) + acc_vec[p1].2-acc_vec[p2].2,
                               acc_vec[p1].2-acc_vec[p2].2);
 
-            let mat = DMatrix3::from_column_vector(3,3,&delta_vec);
-            println!("mat:\n{:?}", mat);
-            println!("m2: {:?}", m2);
+            if m2.determinant() == 0 {
 
-            // if mat.determinant() == 0 {
-            //     println!("collide {:?} and {:?}", p1, p2);
-            // }                      
+                // Will collide - determine time of collision
+                let mut pos1 = pos_vec[p1];
+                let mut vel1 = vel_vec[p1];
+                let mut pos2 = pos_vec[p2];
+                let mut vel2 = vel_vec[p2];
+                let mut t = 0;
+                while pos1 != pos2 {
+                    vel1.0 = vel1.0 + acc_vec[p1].0;
+                    vel1.1 = vel1.1 + acc_vec[p1].1;
+                    vel1.2 = vel1.2 + acc_vec[p1].2;
+                    pos1.0 = pos1.0 + vel1.0;
+                    pos1.1 = pos1.1 + vel1.1;
+                    pos1.2 = pos1.2 + vel1.2;
+
+                    vel2.0 = vel2.0 + acc_vec[p2].0;
+                    vel2.1 = vel2.1 + acc_vec[p2].1;
+                    vel2.2 = vel2.2 + acc_vec[p2].2;
+                    pos2.0 = pos2.0 + vel2.0;
+                    pos2.1 = pos2.1 + vel2.1;
+                    pos2.2 = pos2.2 + vel2.2;
+
+                    t += 1;
+                    if t > 1000 {
+                      t = isize::max_value();
+                      break;
+                    }
+                }
+                let t_c0 = t;
+
+                if t_c0 < collision_t_matrix[p1][p2] {
+                  collision_t_matrix[p1][p2] = t_c0;
+                }
+            }                    
         }
     }
+
+    let mut col_m_cl = collision_t_matrix.clone();
+
+    for p1 in 0..pos_vec.len()-1 {
+
+        for p2 in p1+1..pos_vec.len() {
+          let cp_val = col_m_cl[p1][p2];
+          col_m_cl[p2][p1] = cp_val;
+        }
+    }
+
+    for p1 in 0..pos_vec.len()-1 {
+        let min_val = col_m_cl[p1].iter().min().unwrap().clone();
+        for p2 in p1+1..pos_vec.len() {
+          if col_m_cl[p1][p2] != min_val {
+            col_m_cl[p1][p2] = isize::max_value();
+            col_m_cl[p2][p1] = isize::max_value();
+          }
+        }
+    }
+
+    let n_p = col_m_cl.iter()
+        .filter(|&ln| *ln == vec![isize::max_value(); pos_vec.len()])
+        .count();
+    println!("Number of particles remaining: {:?}", n_p);
 }
